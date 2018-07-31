cd8609a8aa14b74286d889a9f19f3330f92ef344
Romie Silvano <ze.zayn@gmail.com>
2018-07-31 12:00:49 +0800

    updated gitignore file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,8 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   		   scope: "public_profile,email", info_fields: "email,name, first_name, last_name", :display => 'popup'
   }
  
-#  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"], {
-
 
  
 if Rails.env == "production"
@@ -15,7 +13,7 @@ else
     #redirect_uri = 'https://gtmg-seo-valcaro87.c9users.io/auth/google/callback'  #c9 IDE
     redirect_uri = 'http://localhost:3000/auth/google/callback'    #localhost
 end
-    provider :google_oauth2, '4620140787-s1eso31vg0pj9hv01o0rnnlah1mh7rh3.apps.googleusercontent.com', 'JAYTXoUKFweW7jOw0eC0y6wp', {
+    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"], {
     #client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}},
         name: "google", scope: "email, profile", image_aspect_ratio: 'square', image_size: 48, access_type: 'online', skip_jwt: true,
               setup: (lambda do |env|
@@ -26,8 +24,7 @@ end
 
 
   		   
-                      #ENV['LINKEDIN_KEY']  #ENV['LINKEDIN_SECRET']
-  provider :linkedin, '8182jgq6roshzt', 'JsSE81gEDcTEqHNu',{
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'],{
            :scope => 'r_basicprofile r_emailaddress'
   } 			
 end
